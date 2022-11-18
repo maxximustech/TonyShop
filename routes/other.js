d5ae949fbd6d79f42cbd64c2240c758370109ea0
Maxximus <abdullahiazeezbolu@gmail.com>
2022-11-18 15:44:55 +0100

    Updated Project

--- routes/other.js
+++ routes/other.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
+
 //const upload = multer({dest: 'uploads/'});
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/uploads');
+      cb(null, path.join(__dirname,'../uploads'));//
     },
     filename: function (req, file, cb) {
+        let ext1 = file.originalname.split('.');
+        let ext = ext[ext1.length - 1];
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, req.User.username + '-' + uniqueSuffix);
+      cb(null, file.fieldname + '-' + uniqueSuffix+'.'+ext);
     }
   })
 const upload = multer({ storage: storage })
