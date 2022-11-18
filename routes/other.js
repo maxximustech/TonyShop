b68d842f6338611790fed3f6180479d81f0e6be5
Maxximus <abdullahiazeezbolu@gmail.com>
2022-11-18 15:30:49 +0100

    Updated Project

--- routes/other.js
+++ routes/other.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: '../uploads/'});
+//const upload = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, req.User.username + '-' + uniqueSuffix);
+    }
+  })
+const upload = multer({ storage: storage })
 
 router.put('/upload',upload.single('avatar'),async(req,res,next)=>{
     console.log(req.file);
