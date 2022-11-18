10ab5b0afaa3f7644e265258afd37479a6e81f22
Maxximus <abdullahiazeezbolu@gmail.com>
2022-11-18 15:03:59 +0100

    Updated Commit

--- routes/other.js
+++ routes/other.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({dest: '../uploads/'});
 
-router.put('/upload',upload.single('input'),async(req,res,next)=>{
+router.put('/upload',upload.single('avatar'),async(req,res,next)=>{
     console.log(req.file);
 });
 
